4cde391fafce84e8f7faf9e88cdabc22f735096f
liushan <liushan@wustl.edu>
2019-04-07 19:09:34 -0500

    add validation and JWT part

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://ninomi6633:liushan1014@slcluster-pi1rj.mongodb.net/test?retryWrites=true"
+    "mongodb+srv://ninomi6633:liushan1014@slcluster-pi1rj.mongodb.net/test?retryWrites=true",
+  secretOrkey: "secret"
 };
