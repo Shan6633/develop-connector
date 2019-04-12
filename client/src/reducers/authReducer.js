fe36b96797dea3d1949fe9530b2ef707853434d0
liushan <liushan@wustl.edu>
2019-04-12 15:27:52 -0500

    redux store

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+  isAutheticated: false,
+  user: {}
+};
+export default function(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
